9a608fdc0c28d4f710820a530d8c9bf71d7db41d
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-08-02 14:26:10 +0000

    feat: git override salary structure

--- hr_vfg/hr_ventureforce_global/salary_slip_override.py
+++ hr_vfg/hr_ventureforce_global/salary_slip_override.py
@@ -39,10 +39,10 @@ from erpnext.setup.doctype.employee.employee import (
 import erpnext
 from erpnext.accounts.utils import get_fiscal_year
 from hrms.hr.utils import get_holiday_dates_for_employee, validate_active_employee
-from erpnext.loan_management.doctype.loan_repayment.loan_repayment import (
-	calculate_amounts,
-	create_repayment_entry,
-)
+# from erpnext.loan_management.doctype.loan_repayment.loan_repayment import (
+# 	calculate_amounts,
+# 	create_repayment_entry,
+# )
 from hrms.payroll.doctype.additional_salary.additional_salary import get_additional_salaries
 from hrms.payroll.doctype.employee_benefit_application.employee_benefit_application import (
 	get_benefit_component_amount,
