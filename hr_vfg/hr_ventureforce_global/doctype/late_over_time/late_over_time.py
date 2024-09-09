20fbda32b85b6fbc6a7f258410dd29b70d953c94
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-09-09 13:14:32 +0000

    chore: working with ot

--- hr_vfg/hr_ventureforce_global/doctype/late_over_time/late_over_time.py
+++ hr_vfg/hr_ventureforce_global/doctype/late_over_time/late_over_time.py
@@ -1,56 +1,44 @@
 # Copyright (c) 2024, VFG and contributors
 # For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 
 class LateOverTime(Document):
 	@frappe.whitelist()
 	def get_data(self):
-		rec = frappe.db.sql(""" select p.employee,p.employee_name, p.designation, c.late_sitting,c.approved_ot1, c.name as child_name, p.name as parent_name from `tabEmployee Attendance` p
-					  LEFT JOIN `tabEmployee Attendance Table` c ON c.parent=p.name
-					  where p.month=%s and p.year=%s and c.date=%s and c.late_sitting is not  NULL and c.late_sitting > %s
-					  """,
-					  (self.month,self.year,self.date, self.ot_frequency),as_dict=1)
+		# Correct the SQL query to only include the required argument (self.date)
+		rec = frappe.db.sql("""
+			select p.employee, p.employee_name, p.designation, c.late_sitting, c.approved_ot1, c.name as child_name, p.name as parent_name 
+			from `tabEmployee Attendance` p
+			LEFT JOIN `tabEmployee Attendance Table` c ON c.parent=p.name
+			where c.date=%s and c.late_sitting > %s and (c.approved_ot1 = '' or c.approved_ot1 is null) 
+		""", (self.date, self.ot_frequency), as_dict=1)  # Only pass self.date, not self.ot_frequency
 		
-		if len(rec) > 0:
+		if rec:
 			self.details = []
-		for r in rec:
-			allow_ot = frappe.db.get_value('Employee', r.employee, 'is_overtime_allowed')
-			# if allow_ot == 1 and r.approved_ot1 == "00:00:00" and r.late_sitting:
-			if allow_ot == 1 and r.late_sitting:
-				if r.approved_ot1 == "00:00:00":
-			# if r.approved_ot1 == "00:00:00" and r.late_sitting:
-					self.append("details",{
-						"employee":r.employee,
-						"actual_overtime":r.late_sitting,
-						"approved_overtime":r.late_sitting,
-						"employee_name":r.employee_name,
-						"designation":r.designation,
-						"att_ref":r.parent_name,
-						"att_child_ref":r.child_name
-					})
-		self.save()
-
-	# def on_submit(self):
-	# 	for r in self.details:
-	# 		frappe.db.sql("update `tabEmployee Attendance Table` set approved_ot1=%s where name=%s", (r.approved_overtime, r.att_child_ref))
-	# 		frappe.log_error(f"Updating approved_ot1 to {r.approved_overtime} for {r.att_child_ref}")
-	# 		self.reload()
-	# 		frappe.log_error(f"Saved doc {self.name} with updated approved_ot1")
-
-	# 		# frappe.db.sql("update `tabEmployee Attendance Table` set approved_ot1=%s where name=%s",(r.approved_overtime,r.att_child_ref))
-	# 		frappe.db.commit()
-	# 		doc = frappe.get_doc("Employee Attendance",r.att_ref)
-	# 		doc.save()
+			for r in rec:
+				allow_ot = frappe.db.get_value('Employee', r.employee, 'is_overtime_allowed')
+				if allow_ot == 1 and r.late_sitting:
+					# if r.approved_ot1 == "00:00:00":
+						self.append("details", {
+							"employee": r.employee,
+							"actual_overtime": r.late_sitting,
+							"approved_overtime": r.late_sitting,
+							"employee_name": r.employee_name,
+							"designation": r.designation,
+							"att_ref": r.parent_name,
+							"att_child_ref": r.child_name
+						})
+			self.save()
 
 	def on_submit(self):
 		for r in self.details:
+			# Update the `approved_ot1` field in `Employee Attendance Table`
 			frappe.db.sql("update `tabEmployee Attendance Table` set approved_ot1=%s where name=%s", 
-						(r.approved_overtime, r.att_child_ref))
+						  (r.approved_overtime, r.att_child_ref))
 			frappe.db.commit()
 
-			# Reload and update the document
+			# Reload and update the parent document
 			doc = frappe.get_doc("Employee Attendance", r.att_ref)
 			child_row = doc.getone({"name": r.att_child_ref})
 			child_row.approved_ot1 = r.approved_overtime
@@ -59,9 +47,3 @@ class LateOverTime(Document):
 			# Reload to verify update
 			doc.reload()
 			frappe.log_error(f"Updated approved_ot1: {child_row.approved_ot1}")
-
-
-
-
-	
-
