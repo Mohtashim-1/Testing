4b3189618de43af2a6ff8be8085cba5cafeb920e
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-09-12 08:56:13 +0000

    working with overtime slab

--- hr_vfg/hr_ventureforce_global/doctype/over_time_slab/over_time_slab.py
+++ hr_vfg/hr_ventureforce_global/doctype/over_time_slab/over_time_slab.py
@@ -6,6 +6,7 @@ from datetime import datetime, time as datetime_time, timedelta
 class OverTimeSlab(Document):
     def validate(self):
         self.calculate_total_hours()
+        self.per_hours_calculation()
 
     def calculate_total_hours(self):
         FMT = '%H:%M:%S'
@@ -21,6 +22,21 @@ class OverTimeSlab(Document):
                 to_time += timedelta(days=1)
             
             item.total_hours = to_time - from_time
+    
+    def per_hours_calculation(self):
+        for rate in self.over_time_slab_ct:
+            if rate.formula:
+                if rate.formula == "Employee's Overtime Rate":
+                    rate.per_hour_calculation = 1.0
+                if rate.formula == "Employee's Overtime Rate x 1.5":
+                    rate.per_hour_calculation = 1.5
+                if rate.formula == "Employee's Overtime Rate x 2":
+                    rate.per_hour_calculation = 2.0
+                if rate.formula == "Employee Full Day":
+                    rate.per_hour_calculation = self.standard_workinghours
+                if rate.formula == "Employee Full Day x 2":
+                    rate.per_hour_calculation = float(self.standard_workinghours) * 2
+                # frappe.msgprint('123')
 
 
 
