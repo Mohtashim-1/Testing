ab5cb185f691dc9cfc63d9fab15b1b1e3d4adf60
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-09-26 13:56:57 +0000

    feat: cancellation flow added in late ot

--- hr_vfg/hr_ventureforce_global/doctype/late_over_time/late_over_time.py
+++ hr_vfg/hr_ventureforce_global/doctype/late_over_time/late_over_time.py
@@ -8,22 +8,22 @@ class LateOverTime(Document):
 	def get_data(self):
 		# Correct the SQL query to only include the required argument (self.date)
 		rec = frappe.db.sql("""
-			select p.employee, p.employee_name, p.designation, c.late_sitting, c.approved_ot1, c.name as child_name, p.name as parent_name 
+			select p.employee, p.employee_name, p.designation, c.estimated_late, c.approved_ot1, c.name as child_name, p.name as parent_name 
 			from `tabEmployee Attendance` p
 			LEFT JOIN `tabEmployee Attendance Table` c ON c.parent=p.name
-			where c.date=%s and c.late_sitting > %s and (c.approved_ot1 = '' or c.approved_ot1 is null) 
-		""", (self.date, self.ot_frequency), as_dict=1)  # Only pass self.date, not self.ot_frequency
+			where c.date=%s and estimated_late is not null and (c.approved_ot1 = '' or c.approved_ot1 is null) 
+		""", (self.date, ), as_dict=1)  # Only pass self.date, not self.ot_frequency
 		
 		if rec:
 			self.details = []
 			for r in rec:
 				allow_ot = frappe.db.get_value('Employee', r.employee, 'is_overtime_allowed')
-				if allow_ot == 1 and r.late_sitting:
+				if allow_ot == 1 and r.estimated_late:
 					# if r.approved_ot1 == "00:00:00":
 						self.append("details", {
 							"employee": r.employee,
-							"actual_overtime": r.late_sitting,
-							"approved_overtime": r.late_sitting,
+							"actual_overtime": r.estimated_late,
+							"approved_overtime": r.estimated_late,
 							"employee_name": r.employee_name,
 							"designation": r.designation,
 							"att_ref": r.parent_name,
@@ -47,3 +47,20 @@ class LateOverTime(Document):
 			# Reload to verify update
 			doc.reload()
 			frappe.log_error(f"Updated approved_ot1: {child_row.approved_ot1}")
+	
+	def on_cancel(self):
+		for r in self.details:
+			# Update the `approved_ot1` field in `Employee Attendance Table`
+			frappe.db.sql("update `tabEmployee Attendance Table` set approved_ot1='' where name=%s", 
+						  (r.att_child_ref,))
+			frappe.db.commit()
+
+			doc = frappe.get_doc("Employee Attendance", r.att_ref)
+			child_row = doc.getone({"name": r.att_child_ref})
+			child_row.approved_ot1 = ''
+			doc.save()
+
+			# Reload to verify update
+			doc.reload()
+			frappe.log_error(f"Updated approved_ot1: {child_row.approved_ot1}")
+
