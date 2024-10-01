1e59e77f5aa060567ee21f649fbedfc44130c998
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-10-01 06:54:23 +0000

    chore: enable auto attendnace fetch

--- hr_vfg/hooks.py
+++ hr_vfg/hooks.py
@@ -106,16 +106,16 @@ doc_events = {
 # ---------------
 
 scheduler_events = {
-    "daily": [
-        "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_from_hook"
-    ]
+    # "daily": [
+    #     "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_from_hook"
+    # ]
     # working
     # "all": [
     #     "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_from_hook"
     # ],
-    # "hourly": [
-    #     "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_from_hook"
-    # ]
+    "hourly": [
+        "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_from_hook"
+    ]
 }
 
 
