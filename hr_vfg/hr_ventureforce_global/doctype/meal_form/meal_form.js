6453295d6f276d5a98e6943a5eda1f5e8d815850
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-10-11 13:38:35 +0000

    fix:fix

--- hr_vfg/hr_ventureforce_global/doctype/meal_form/meal_form.js
+++ hr_vfg/hr_ventureforce_global/doctype/meal_form/meal_form.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2024, VFG and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Meal Form", {
-// 	refresh(frm) {
+frappe.ui.form.on("Meal Form", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
