88770b84d9a10fa1b022b53320cdb09f828bb20a
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-11-01 10:03:19 +0000

    chore: meal form added with calculation of categories

--- hr_vfg/hr_ventureforce_global/doctype/meal_form/meal_form.py
+++ hr_vfg/hr_ventureforce_global/doctype/meal_form/meal_form.py
@@ -3,43 +3,108 @@
 
 import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
 
 class MealForm(Document):
 	def validate(self):
-		self.calculate_total_contractor()
-		self.calculate_total_employee()
-		self.calculate_total_sum()
-		self.rate_of_meal()
-		self.sum_total_amount()
-
-	def calculate_total_contractor(self):
-		total = 0
-		for i in self.detail:
-			total += i.quantity
-		self.total_contractor = total
+		# self.calculate_total_contractor()
+		# self.calculate_total_employee()
+		# self.calculate_total_sum()
+		# self.rate_of_meal()
+		# self.sum_total_amount()
+		self.contract_rate_base_on_category()
+		self.employee_rate_base_on_category()
+		self.total_qty_and_total_amount()
 
 	
-	def calculate_total_employee(self):
-		total1 = 0
-		for j in self.detail_meal:
-			total1 += j.qty
-		self.total_employees = total1
-	
-	def calculate_total_sum(self):
-		total_qty = self.total_contractor + self.total_employees
-		self.total_qty = total_qty
-
-	def rate_of_meal(self):
-		meal_provider = frappe.get_doc("Meal Provider",self.meal_provider)
+	def contract_rate_base_on_category(self):
+		meal_provider = frappe.get_doc("Meal Provider", self.meal_provider)
 		meal_data = meal_provider.meal_provider_ct
-		for m in meal_data:
-			if self.meal_type == m.meal_type:
-				self.per_meal_rate = m.rate
 
-	def sum_total_amount(self):
-		total_amount = self.per_meal_rate * self.total_qty
-		self.total_amount = total_amount
+		date = datetime.strptime(self.date, "%Y-%m-%d").date() if isinstance(self.date, str) else self.date
+
+		for m in meal_data:
+			for j in self.detail:
+				from_date = datetime.strptime(m.from_date, "%Y-%m-%d").date() if isinstance(m.from_date, str) else m.from_date
+				to_date = datetime.strptime(m.to_date, "%Y-%m-%d").date() if isinstance(m.to_date, str) else m.to_date
+
+				if from_date <= date <= to_date:
+					if m.category == j.meal_category:
+						if m.meal_type == self.meal_type:
+							j.rate = m.rate
+							j.amount = j.rate * j.quantity
+				else:
+					j.rate = 0
+					j.amount = 0
+
+	def employee_rate_base_on_category(self):
+		meal_provider = frappe.get_doc("Meal Provider", self.meal_provider)
+		meal_data = meal_provider.meal_provider_ct
+
+		date = datetime.strptime(self.date, "%Y-%m-%d").date() if isinstance(self.date, str) else self.date
+
+		for m in meal_data:
+			for j in self.detail_meal:
+				from_date = datetime.strptime(m.from_date, "%Y-%m-%d").date() if isinstance(m.from_date, str) else m.from_date
+				to_date = datetime.strptime(m.to_date, "%Y-%m-%d").date() if isinstance(m.to_date, str) else m.to_date
+
+				if from_date <= date <= to_date:
+					if m.category == j.meal_category:
+						if m.meal_type == self.meal_type:
+							j.rate = m.rate
+							j.amount = j.rate * j.qty
+				else:
+					j.rate = 0
+					j.amount = 0
+	
+	def total_qty_and_total_amount(self):
+		total_qty = 0
+		total_amount = 0
+		total_qty1 = 0
+		total_amount1 = 0
+		for j in self.detail:
+			total_qty += j.quantity
+			total_amount += j.amount
+		for i in self.detail_meal:
+			total_qty1 += i.qty
+			total_amount1 += i.amount
+		self.total_contractor = total_qty
+		self.total_contract_amount = total_amount
+		self.total_employees = total_qty1
+		self.total_employee_amount = total_amount1
+		self.total_qty = self.total_contractor + self.total_employees 
+		self.total_amount = self.total_contract_amount + self.total_employee_amount
+				
+
+
+	# def calculate_total_contractor(self):
+	# 	total = 0
+	# 	for i in self.detail:
+	# 		total += i.quantity
+	# 	self.total_contractor = total
+
+	
+	# def calculate_total_employee(self):
+	# 	total1 = 0
+	# 	for j in self.detail_meal:
+	# 		total1 += j.qty
+	# 	self.total_employees = total1
+	
+	# def calculate_total_sum(self):
+	# 	total_qty = self.total_contractor + self.total_employees
+	# 	self.total_qty = total_qty
+
+	# # def rate_of_meal(self):
+	# # 	meal_provider = frappe.get_doc("Meal Provider",self.meal_provider)
+	# # 	meal_data = meal_provider.meal_provider_ct
+	# # 	for m in meal_data:
+	# # 		if self.meal_type == m.meal_type:
+	# # 			self.per_meal_rate = m.rate
+
+	# def sum_total_amount(self):
+	# 	total_amount = self.per_meal_rate * self.total_qty
+	# 	self.total_amount = total_amount
 
 
 		
