23c273ffc81b0fd1ad90cfc81a87b16de6b756cc
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-11-15 20:02:31 +0000

    fix: fix in employee attendance

--- hr_vfg/hr_ventureforce_global/doctype/employee_attendance/employee_attendance_list.js
+++ hr_vfg/hr_ventureforce_global/doctype/employee_attendance/employee_attendance_list.js
@@ -1,79 +1,55 @@
 frappe.listview_settings['Employee Attendance'] = {
-	colwidths: {"subject": 6},
-	onload: function(listview) {
-	
-		var methods = "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_long";
-        
-				listview.page.add_menu_item(__("Get Attendance"), function() {
-					var dialog = new frappe.ui.Dialog({
-						title: __('Add Follow Up'),
-						fields: [
-				
-							{ fieldtype: 'Date', reqd:1, fieldname: 'from_date', label: __("From Date"),default: frappe.datetime.add_months(frappe.datetime.get_today(), -1), },
-							{ fieldtype: 'Column Break' },
-							{ fieldtype: 'Date',reqd:1, fieldname: 'to_date', label: __("To Date"), default: frappe.datetime.get_today(),},
-							{ fieldtype: 'Section Break' },
-				
-							{ fieldtype: 'Link', fieldname: 'employee', label: __("Employee"),options:"Employee" },
-							{ fieldtype: 'Link', fieldname: 'department', label: __("Department"),options:"Department" },
-				
-				
-						],
-						primary_action: function () {
-							var args = dialog.get_values();
-							console.log(args)
-							listview.call_for_selected_items(methods, args);
-							dialog.hide()
-						},
-						primary_action_label: __("Submit")
-					})
-					dialog.show()
-			
-		});
-
-		//var methods = "hr_vfg.hr_ventureforce_global.doctype.attendance_logs.attendance_logs.sync_attendance";
-       
-		// listview.page.add_menu_item(__("Generate Attendance"), function() {
-		// 	listview.call_for_selected_items(method, {"status": "Open"});
-		// });
-
-
-		// 		listview.page.add_menu_item(__("Sync Manual Attendance"), function() {
-		// 			var dialog = new frappe.ui.Dialog({
-		// 				title: __('Filters'),
-		// 				fields: [
-				
-		// 					{ fieldtype: 'Date', reqd:1, fieldname: 'from_date', label: __("From Date") },
-		// 					{ fieldtype: 'Column Break' },
-		// 					{ fieldtype: 'Date',reqd:1, fieldname: 'to_date', label: __("To Date") },
-		// 					{ fieldtype: 'Section Break' },
-				
-		// 					{ fieldtype: 'Link', fieldname: 'employee', label: __("Employee"),options:"Employee" },
-		// 					{ fieldtype: 'Link', fieldname: 'department', label: __("Department"),options:"Department" },
-				
-				
-		// 				],
-		// 				primary_action: function () {
-		// 					var args = dialog.get_values();
-		// 					console.log(args)
-		// 					listview.call_for_selected_items(methods, args);
-		// 					dialog.hide()
-		// 				},
-		// 				primary_action_label: __("Submit")
-		// 			})
-		// 			dialog.show()
-			
-		// });
-
-
-	
-}
-}
-
-
-
-frappe.listview_settings['Employee Attendance'] = {
+    colwidths: { "subject": 6 },
     onload: function(listview) {
+        // Add "Get Attendance" menu item
+        listview.page.add_menu_item(__('Get Attendance'), function() {
+            var dialog = new frappe.ui.Dialog({
+                title: __('Get Attendance'),
+                fields: [
+                    { 
+                        fieldtype: 'Date', 
+                        reqd: 1, 
+                        fieldname: 'from_date', 
+                        label: __("From Date"), 
+                        default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+                    },
+                    { fieldtype: 'Column Break' },
+                    { 
+                        fieldtype: 'Date',
+                        reqd: 1, 
+                        fieldname: 'to_date', 
+                        label: __("To Date"), 
+                        default: frappe.datetime.get_today(),
+                    },
+                    { fieldtype: 'Section Break' },
+                    { 
+                        fieldtype: 'Link', 
+                        fieldname: 'employee', 
+                        label: __("Employee"),
+                        options: "Employee" 
+                    },
+                    { 
+                        fieldtype: 'Link', 
+                        fieldname: 'department', 
+                        label: __("Department"),
+                        options: "Department" 
+                    },
+                ],
+                primary_action: function () {
+                    var args = dialog.get_values();
+                    console.log(args);
+                    listview.call_for_selected_items(
+                        "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.attendance_connector.get_attendance_long", 
+                        args
+                    );
+                    dialog.hide();
+                },
+                primary_action_label: __("Submit")
+            });
+            dialog.show();
+        });
+
+        // Add "Refresh" action item
         listview.page.add_action_item(__('Refresh'), function() {
             // Get selected documents
             const selected_docs = listview.get_checked_items();
@@ -84,19 +60,29 @@ frappe.listview_settings['Employee Attendance'] = {
 
             // Confirm action
             frappe.confirm(
-                __('Are you sure you want to refresh the table for selected records?'),
+                __('Are you sure you want to refresh the table for the selected records?'),
                 function() {
-                    frappe.call({
-                        method: "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.employee_attendance.refresh_table",
-                        args: {
-                            docname: selected_docs[0].name // Process the first selected doc (you can loop if required)
-                        },
-                        callback: function(response) {
-                            if (response.message) {
-                                frappe.msgprint(response.message);
-                                listview.refresh(); // Refresh the List View
+                    let processed_count = 0; // Counter to track completion
+
+                    // Loop through all selected docs
+                    selected_docs.forEach(doc => {
+                        frappe.call({
+                            method: "hr_vfg.hr_ventureforce_global.doctype.employee_attendance.employee_attendance.refresh_table",
+                            args: {
+                                docname: doc.name
+                            },
+                            callback: function(response) {
+                                processed_count++;
+                                if (response.message) {
+                                    frappe.msgprint(`Refreshed ${doc.name}: ${response.message}`);
+                                }
+
+                                // Refresh List View after all calls are complete
+                                if (processed_count === selected_docs.length) {
+                                    listview.refresh();
+                                }
                             }
-                        }
+                        });
                     });
                 }
             );
