4e17e9b8638e970be78d85567a2e42fba1812107
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2026-02-24 15:49:51 +0500

    chore: update

--- hr_vfg/hr_ventureforce_global/doctype/meal_form/meal_form.js
+++ hr_vfg/hr_ventureforce_global/doctype/meal_form/meal_form.js
@@ -2,8 +2,7 @@ frappe.ui.form.on('Meal Form', {
     meal_type: function (frm) {
         console.log('Meal Type Selected: ', frm.doc.meal_type); // Debugging
 
-        if (frm.doc.meal_type === 'Breakfast' || frm.doc.meal_type === 'Lunch' || frm.doc.meal_type === 'Dinner' || frm.doc.meal_type === 'Iftari' && 
-            frm.doc.meal_type === 'Sehri') {
+        if (frm.doc.meal_type === 'Breakfast' || frm.doc.meal_type === 'Lunch' || frm.doc.meal_type === 'Dinner' || frm.doc.meal_type === 'Iftari' || frm.doc.meal_type === 'Sehri') {
             // Show child tables
             frm.toggle_display('detail', true);
             frm.toggle_display('detail_meal', true);
